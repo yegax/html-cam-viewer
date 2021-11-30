c5940f610b080af395844ab89e98302cd0bc5b57
yegax <valdesgpatricio@gmail.com>
2021-11-30 17:19:32 -0500

    Updates on UI Design

--- filter/cool.js
+++ filter/cool.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
   $("#effect-cool").click(function() {
+    $("#videoElement").removeClass();
     $("#videoElement").toggleClass("cool");
+    $("#effect-cool").toggleClass("buttonElement:active");
     });
     
   });
